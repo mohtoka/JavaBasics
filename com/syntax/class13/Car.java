29693ce91dd7030214964652c0b6dbb916e7d4d7
mohtoka <mohamedtokaa@gmail.com>
2020-07-12 00:51:09 -0400

    some changes

--- com/syntax/class13/Car.java
+++ com/syntax/class13/Car.java
@@ -7,5 +7,17 @@ public class Car {
 	String color;
 	int year;
 	int speed;
+	
+	void drive() {
+		System.out.println("Car can drive");
+	}
+	
+	void reverse() {
+		System.out.println("Car can reverse");
+	}
+	
+	void transportPeople() {
+		System.out.println("Car can transport people");
+	}
 
 }
--- com/syntax/class13/CarFactory.java
+++ com/syntax/class13/CarFactory.java
@@ -4,6 +4,8 @@ public class CarFactory {
 
 	public static void main(String[] args) {
 
+		System.out.println("-----------Car 1-----------");
+
 		Car c1 = new Car();
 		c1.make = "Tesla";
 		c1.model = "X";
@@ -16,6 +18,13 @@ public class CarFactory {
 							" and color is " + c1.color + 
 							" and year is " + c1.year + 
 							" and speed is " + c1.speed);
+		
+		c1.drive();
+		c1.reverse();
+		c1.transportPeople();
+		
+		System.out.println("-----------Car 2-----------");
+		
 		Car c2 = new Car();
 		c2.make = "BMW";
 		c2.model = "A";
@@ -29,6 +38,12 @@ public class CarFactory {
 							" and year is " + c2.year + 
 							" and speed is " + c2.speed);
 		
+		c2.drive();
+		c2.reverse();
+		c2.transportPeople();
+		
+		System.out.println("-----------Car 3-----------");
+
 		Car c3 = new Car();
 		c3.make = "Toyota";
 		c3.model = "Camry";
@@ -41,6 +56,12 @@ public class CarFactory {
 							" and color is " + c3.color + 
 							" and year is " + c3.year + 
 							" and speed is " + c3.speed);
+		
+		c3.drive();
+		c3.reverse();
+		c3.transportPeople();
+		
+		System.out.println("-----------Car 4-----------");
 		
 		Car c4 = new Car();
 		c4.make = "Volvo";
@@ -55,6 +76,9 @@ public class CarFactory {
 							" and year is " + c4.year + 
 							" and speed is " + c4.speed);
 		
+		c4.drive();
+		c4.reverse();
+		c4.transportPeople();
 	}
 
 }
