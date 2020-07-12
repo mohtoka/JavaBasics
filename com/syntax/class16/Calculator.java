ca7acc1180a5071ade87d6bbec050136b2708ccb
mohtoka <mohamedtokaa@gmail.com>
2020-07-12 16:51:24 -0400

    calc

---
+++ com/syntax/class16/Calculator.java
@@ -0,0 +1,33 @@
+package com.syntax.class16;
+
+public class Calculator {
+
+//	void add() {
+//		int a = 10;
+//		int b = 20;
+//
+//		System.out.println(a + b);
+//	}
+
+	void add(int a, int b) {
+		System.out.println(a + b);
+	}
+	
+	//
+	
+	void sub (int a, int b) {
+		System.out.println(a - b);
+	}
+	
+	void mult (int a, int b) {
+		System.out.println(a * b);
+	}
+	
+	void div (int a, int b) {
+		System.out.println(a / b);
+	}
+	
+	
+	
+
+}
---
+++ com/syntax/class16/CalculatorTest.java
@@ -0,0 +1,19 @@
+package com.syntax.class16;
+
+public class CalculatorTest {
+	
+	public static void main(String[] args) {
+		
+		Calculator c = new Calculator();
+		
+		//c.add();
+		c.add(11, 22);
+
+		c.sub(20, 5);
+		
+		c.mult(10, 3);
+
+		c.div(40, 5);
+	}
+
+}
