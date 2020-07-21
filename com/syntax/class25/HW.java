9076ed69e05250c140fbcc1edb528f9662846673
mohtoka <mohamedtokaa@gmail.com>
2020-07-21 18:53:10 -0400

    Class 25 Mod

--- com/syntax/class25/HW.java
+++ com/syntax/class25/HW.java
@@ -1,5 +1,21 @@
 package com.syntax.class25;
 
 public class HW {
+	
+	final void hello(String str) {
+		System.out.println("String");
+	}
+	
+	final void hello(int i) {
+		System.out.println("int");
+	}
+	
+	final void hello(boolean b) {
+		System.out.println("boolean");
+	}
+	
+	final void hello(String[] str) {
+		System.out.println("String array");
+	}
 
 }
